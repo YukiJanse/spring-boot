4daa40a0bed2b46f9c85cee40390d6bf214d63ce
YukiJanse <yuki.janse@gmail.com>
2026-02-01 21:09:16 +0100

    Added util classes

---
+++ src/main/java/se/jensen/yuki/springboot/util/CurrentUserProvider.java
@@ -0,0 +1,21 @@
+package se.jensen.yuki.springboot.util;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import se.jensen.yuki.springboot.repository.UserRepository;
+
+@Component
+@RequiredArgsConstructor
+public class CurrentUserProvider {
+    private final UserRepository userRepository;
+
+    public Long getCurrentUserId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"))
+                .getId();
+    }
+}
