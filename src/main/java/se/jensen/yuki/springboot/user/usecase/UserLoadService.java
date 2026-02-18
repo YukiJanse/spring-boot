1525842c4572e296f1d1ebc6700658c2a3e72599
YukiJanse <yuki.janse@gmail.com>
2026-02-18 20:21:09 +0100

    Fixed wrong documentation

--- src/main/java/se/jensen/yuki/springboot/user/usecase/UserLoadService.java
+++ src/main/java/se/jensen/yuki/springboot/user/usecase/UserLoadService.java
@@ -11,7 +11,7 @@ public interface UserLoadService {
      * Loads a user by their unique identifier.
      *
      * @param id the unique identifier of the user
-     * @return the UserJpaEntity corresponding to the given id, or null if not found
+     * @return the UserJpaEntity corresponding to the given id, or throw an exception if not found
      */
     UserJpaEntity requireJpaById(Long id);
 
@@ -19,7 +19,7 @@ public interface UserLoadService {
      * Loads a user by their email address.
      *
      * @param email the email address of the user
-     * @return the UserJpaEntity corresponding to the given email, or null if not found
+     * @return the UserJpaEntity corresponding to the given email, or throw an exception if not found
      */
     UserJpaEntity requireJpaByEmail(String email);
 }
